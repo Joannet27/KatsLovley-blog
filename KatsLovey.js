010a892990e1a9ab369f81d3f235a5c0c11e057a
Joannet27 <tran.joanne27@gmail.com>
2026-06-24 17:20:58 -0400

    Masonry jquery inputted but ran into problem

---
+++ KatsLovey.js
@@ -0,0 +1,22 @@
+// copy over older code of the original Katslovely js project
+
+// ------ Sparkle trail effect ------
+window.addEventListener('mousemove', function(e) {
+    let arr = [1,0.9,0.8,0.5,0.2];// sparkle density
+
+    arr.forEach(function(i){
+        let x =(1-i)* 15;// change the rail cluster
+        let spark = document.createElement('div');
+        // show spark on the possition of the sursor location and dispear after a certain time
+        spark.className = 'spark';
+        spark.style.left = e.pageX + Math.round(Math.random() * x - x / 2)+ 'px';
+        spark.style.top = e.pageY + Math.round(Math.random() * x - x / 2)+ 'px';
+       
+        document.body.appendChild(spark);
+        window.setTimeout(function() {
+            document.body.removeChild(spark);
+        },Math.round(Math.random()* i* 600)); // spark removed after 600 milliseconds
+    });
+},false);
+
+// ---- end of sparkle trail effect  code------
